c0fca86fde36f5b84e75b91c4467bc6f7f3b0b1f
Olalekan Ayodele <longyarnz@gmail.com>
2019-02-18 19:03:03 +0100

    Add more form inputs to registration form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,7 @@ import Icon from '../components/Icon';
 
 function submitLoginForm(e, dispatch, goTo) {
   e.preventDefault();
-  const [ email ] = e.target;
+  const [email] = e.target;
 
   dispatch({
     type: 'LOG USER IN',
@@ -21,37 +21,26 @@ export default function Login(props) {
   const onSubmit = e => submitLoginForm(e, props.dispatch, props.goTo);
 
   return (
-    <section className="login register">
-      <div className="container">
-        <div className="banner-content">
-          <button onClick={() => props.goTo('./pages/NewsFeed')}>
-            <Icon name="home" />
-          </button>
-          <img src="assets/img/favicon.png" alt="Dominerf Logo" style={{ width: '30%' }} />
-          <form method="post" className="form-signin" onSubmit={onSubmit}>
-            <h3 className="form-signin-heading">We've Missed You!</h3>
-            <div className="form-group">
-              <input name="email" type="email" className="form-control" placeholder="Email" />
-            </div>
-            <div className="form-group">
-              <input type="password" className="form-control" name="password" placeholder="Password" />
-            </div>
-            <button className="kafe-btn kafe-btn-mint btn-block" type="submit" name="subm">LOG IN</button>
-            <br />
-            <span className="btn btn-dark " role="button" onClick={() => props.goTo('./pages/Register')}>
-              Don't have an account?
-              <button>
-                REGISTER
-              </button>
-            </span>
-            <span className="btn btn-dark " role="button" onClick={() => props.goTo('./pages/Register')}>
-              <button>
-                FORGOT YOUR PASSWORD?
-              </button>
-            </span>
-          </form>
-        </div>
-      </div>
+    <section className="register">
+      <button onClick={() => props.goTo('./pages/NewsFeed')}>
+        <Icon name="home" />
+      </button>
+      <img src="assets/img/favicon.png" alt="Dominerf Logo" />
+      <form method="post" onSubmit={onSubmit}>
+        <h3>We've Missed You!</h3>
+        <input name="email" type="email" placeholder="Email" />
+        <input type="password" name="password" placeholder="Password" />
+        <button className="kafe-btn kafe-btn-mint btn-block" type="submit" name="subm">LOG IN</button>
+
+        <span className="btn btn-dark " role="button" onClick={() => props.goTo('./pages/Register')}>
+          Don't have an account?
+          <button>REGISTER</button>
+        </span>
+
+        <span className="btn btn-dark " role="button" onClick={() => props.goTo('./pages/Register')}>
+          <button>FORGOT YOUR PASSWORD?</button>
+        </span>
+      </form>
     </section>
   )
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -22,39 +22,35 @@ export default function Register(props) {
 
   return (
     <section className="register">
-      <div className="container">
-        <div className="banner-content">
-          <button onClick={() => props.goTo('./pages/NewsFeed')}>
-            <Icon name="home" />
-            {/* <span>HOME</span> */}
-          </button>
-          <img src="assets/img/favicon.png" alt="Dominerf Logo" />
-          <form method="post" className="form-signin" onSubmit={onSubmit}>
-            <h3 className="form-signin-heading">Nice to Meet Ya!</h3>
-            <div className="form-group">
-              <input name="first" type="text" className="form-control" placeholder="First Name" autoComplete="true" />
-            </div>
-            <div className="form-group">
-              <input name="last" type="text" className="form-control" placeholder="Last Name" autoComplete="true" />
-            </div>
-            <div className="form-group">
-              <input name="email" type="email" className="form-control" placeholder="Email" autoComplete="true" />
-            </div>
-            <div className="form-group">
-              <input type="password" className="form-control" name="password" placeholder="Password" autoComplete="true" />
-            </div>
-            <button className="kafe-btn kafe-btn-mint btn-block" type="submit" name="subm">SIGN UP</button>
-            <br />
-            <span className="btn btn-dark " role="button" onClick={() => props.goTo('./pages/Login')}>
-              Already have an account?
-              <button>
-                LOG IN
-              </button>
-            </span>
-          </form>
+      <button onClick={() => props.goTo('./pages/NewsFeed')}>
+        <Icon name="home" />
+      </button>
+      <img src="assets/img/favicon.png" alt="Dominerf Logo" />
 
-        </div>
-      </div>
+      <form method="post" onSubmit={onSubmit}>
+        <h3>Nice to Meet Ya!</h3>
+        <input name="first" type="text" placeholder="First Name" autoComplete="true" />
+        <input name="last" type="text" placeholder="Last Name" autoComplete="true" />
+        <input name="email" type="email" placeholder="Email" autoComplete="true" />
+        <input type="password" name="password" placeholder="Password" autoComplete="true" />
+        <input type="password" name="password" placeholder="Confirm Password" />
+        <input list="sexes" name="sex" placeholder="Sex" />
+        <datalist id="sexes">
+          <option value="Male" />
+          <option value="Female" />
+        </datalist>
+        <input list="accounts" name="accountType" placeholder="Account Type" />
+        <datalist id="accounts">
+          <option value="Fashion Designer" />
+          <option value="Fashion Enthusiast" />
+        </datalist>
+        <button className="kafe-btn kafe-btn-mint btn-block" type="submit" name="subm">SIGN UP</button>
+        <br />
+        <span className="btn btn-dark " role="button" onClick={() => props.goTo('./pages/Login')}>
+          Already have an account?
+          <button>LOG IN</button>
+        </span>
+      </form>
     </section>
   )
 }
