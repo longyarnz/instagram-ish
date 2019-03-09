92323ba2329632b4bda274c19fb7c76c87fcb993
Olalekan Ayodele <longyarnz@gmail.com>
2019-03-09 11:04:47 +0100

    Increase the height of the nav bar

--- src/components/SandwichMenu.js
+++ src/components/SandwichMenu.js
@@ -6,9 +6,9 @@ export default function SandwichMenu (props) {
 
   const sandwich = (
     <SVG onClick={onClick} attr="sandwich" style={style}>
-      <rect width="100%" height={2} y={2} />
-      <rect width="80%" height="2px" y={9} />
-      <rect width="100%" height="2px" y={16} />
+      <rect width="100%" height={2} y={3} />
+      <rect width="80%" height="2px" y={12} />
+      <rect width="100%" height="2px" y={21} />
     </SVG>
   );
 
